0c55efb05d79f6974291d1ff86f4ee84c20ade56
szymon <sz.kozakiewicz@gmail.com>
2025-11-08 00:53:52 +0100

    add landing page design

--- src/app/domains/landing/pages/landing/landing.ts
+++ src/app/domains/landing/pages/landing/landing.ts
@@ -1,11 +1,30 @@
 import { Component } from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+import {MatFormFieldModule} from '@angular/material/form-field';
+
+interface Food {
+  value: string;
+  viewValue: string;
+}
+
+interface Car {
+  value: string;
+  viewValue: string;
+}
 
 @Component({
   selector: 'app-landing',
-  imports: [],
+  imports: [MatSelectModule, MatFormFieldModule, MatInputModule],
   templateUrl: './landing.html',
   styleUrl: './landing.css',
 })
-export class Landing {
+export class Landing  {
+   
+
+
+
+  
 
 }
